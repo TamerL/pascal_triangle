c80be87947a6a09deaae4a237c0dbf7fad09da17
tamerl <eng.tamereid@gmail.com>
2020-01-05 22:59:20 +1100

    Adjusting the spaces alignment for high triangle levels

--- pascal_triangle.rb
+++ pascal_triangle.rb
@@ -1,18 +1,43 @@
-  puts "Please enter a level for the pascalpyramid to be printed:"
-  level = gets.chomp(level).to_i
-  # puts "level #{level}"
+
+def build_Pascal_triangle(level)
   iterator = 0
-  tobe_printed = []
+  pascal_triangle = []
   while iterator < level do
-    tobe_printed[iterator] = "1"
+    pascal_triangle[iterator] = [1]
     if iterator > 1
       horiz = 1
-      while horiz < tobe_printed[iterator - 1].split("").length do
-        tobe_printed[iterator] << (tobe_printed[iterator - 1].split("")[horiz-1].to_i + tobe_printed[iterator - 1].split("")[horiz].to_i).to_s
+      while horiz < pascal_triangle[iterator - 1].length do
+        pascal_triangle[iterator] << pascal_triangle[iterator - 1][horiz-1] + pascal_triangle[iterator - 1][horiz]
         horiz += 1
       end
     end
-    tobe_printed[iterator] << "1" if iterator > 0
-    puts tobe_printed[iterator].chars.join(" ").center(2 * level)
+    pascal_triangle[iterator] << 1 if iterator > 0
     iterator += 1
   end
+
+  return pascal_triangle
+end
+
+
+def print_pascal_triangle
+  puts "Please enter a level for the pascalpyramid to be printed:"
+  level = gets.chomp(level).to_i
+  pascal_triangle = build_Pascal_triangle(level)
+  iterator = 0
+  puts "pascal #{pascal_triangle}"
+  max_width = pascal_triangle[-1].max.to_s.length
+  # join().split().length
+  puts max_width + 1
+  pascal_triangle.map!{|row| row.map{|i| i.to_s.center(max_width*2)}.join }
+  max_length = pascal_triangle[-1].length
+
+  pascal_triangle.each do |i|
+    puts i.center(max_length)
+  end
+  # while iterator < level do
+  #   puts pascal_triangle[iterator].map{|i| i.center(max_width * 2)}.join().center(max_length +  level)
+  #   iterator += 1
+  # end
+end
+
+print_pascal_triangle
