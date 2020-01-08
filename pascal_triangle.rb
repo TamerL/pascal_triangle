8fa80560c9b1ca48699526cb762c8947860f7a24
tamerl <eng.tamereid@gmail.com>
2020-01-08 12:21:52 +1100

    Refactoring Pascal triangle building method

--- pascal_triangle.rb
+++ pascal_triangle.rb
@@ -1,19 +1,8 @@
 def build_Pascal_triangle(level)
-  iterator = 0
-  pascal_triangle = []
-  while iterator < level do
-    pascal_triangle[iterator] = [1]
-    if iterator > 1
-      horiz = 1
-      while horiz < pascal_triangle[iterator - 1].length do
-        pascal_triangle[iterator] << pascal_triangle[iterator - 1][horiz-1] + pascal_triangle[iterator - 1][horiz]
-        horiz += 1
-      end
-    end
-    pascal_triangle[iterator] << 1 if iterator > 0
-    iterator += 1
+  pascal_triangle = [[1]]
+  (1..level).each do
+    pascal_triangle << pascal_triangle[-1].inject([0]) { |inj,val| inj[-1] = inj[-1] + val ; inj << val ; inj }
   end
-
   return pascal_triangle
 end
 
@@ -23,20 +12,12 @@ def print_pascal_triangle
   level = gets.chomp(level).to_i
   pascal_triangle = build_Pascal_triangle(level)
   iterator = 0
-  # puts "pascal #{pascal_triangle}"
   max_width = pascal_triangle[-1].max.to_s.length
-  # join().split().length
-  # puts max_width + 1
   pascal_triangle.map!{|row| row.map{|i| i.to_s.center(max_width*2)}.join }
   max_length = pascal_triangle[-1].length
-
   pascal_triangle.each do |i|
     puts i.center(max_length)
   end
-  # while iterator < level do
-  #   puts pascal_triangle[iterator].map{|i| i.center(max_width * 2)}.join().center(max_length +  level)
-  #   iterator += 1
-  # end
 end
 
 print_pascal_triangle
